237fb96fd805c81ad2140c8a075b1ebe92a0d5ca
ardijr <101171711+ardijr@users.noreply.github.com>
2022-03-09 13:48:15 +0700

    Add files via upload
    latihan spring boot

---
+++ PBO/demo/src/main/java/id/ac/uin/demo/ArdiApplication.java
@@ -0,0 +1,13 @@
+package id.ac.uin.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArdiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArdiApplication.class, args);
+	}
+
+}
---
+++ PBO/demo/src/test/java/id/ac/uin/demo/ArdiApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.uin.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArdiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
